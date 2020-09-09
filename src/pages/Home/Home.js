1b1bf1c705777b0721ee54897547d6eb86b5d4f7
Carolina Klein <carolina@Carolina-rMBP.local>
2020-09-09 12:33:50 -0400

    initial repo organization

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Home() {
+
+    return (
+        <div className="nick-home">
+            <div className="nick-home-title">Aptentaciti sociosqu!</div>
+            <div className="nick-home-description">Litora torquent per conubia nostra, per inceptos himenaeos. Donec vel erat maximus, lacinia arcu vel, interdum turpis. Vivamus a eleifend nisi.</div>
+            <div className="columns nick-home-cards"></div>
+            <div className="column nick-home-card">card</div>
+        </div>
+    );
+}
+export default Home;
