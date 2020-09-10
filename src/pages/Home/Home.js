946b34bbefed9212f6fcd8f00090d38e7044c075
Carolina Klein <carolina@Carolina-rMBP.local>
2020-09-10 18:03:02 -0400

    conted added to home page with bulma column classes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,46 @@
 import React from 'react';
-
+import '../Home/Home.scss'
 
 function Home() {
 
+    const videos = [1, 2, 3]
+
     return (
         <div className="nick-home">
-            <div className="nick-home-title">Aptentaciti sociosqu!</div>
-            <div className="nick-home-description">Litora torquent per conubia nostra, per inceptos himenaeos. Donec vel erat maximus, lacinia arcu vel, interdum turpis. Vivamus a eleifend nisi.</div>
-            <div className="columns nick-home-cards"></div>
-            <div className="column nick-home-card">card</div>
+            <div className="columns is-centered">
+                <div className="column is-half has-text-centered">
+                    <h1 className="nick-home-title">
+                      Aptentaciti sociosqu!
+                    </h1>
+                    <p className="nick-home-description">
+                    Litora torquent per conubia nostra, per inceptos himenaeos. Donec vel erat maximus, lacinia arcu vel, interdum turpis. Vivamus a eleifend nisi.
+                    </p>
+                </div>
+            </div>
+            <div className="columns is-centered">
+                    {videos.map((video) => {
+                        return (
+                            <div className="column is-one-quarter has-text-centered" key={video}>
+                                <div className="nick-home-cards">
+                                        <div>
+                                            <img src={video.firstLogo}/>
+                                            <img src={video.secondLogo}/>
+                                        </div>
+                                        <img src={video.thumbnail}></img>
+                                        <button>Watch Video</button>
+                                    </div>
+                          </div>
+                        )
+                    })}   
+            </div>
+            <div className="columns is-centered">
+                <div className="column  is-three-quarters has-text-centered">
+                    <div className="nick-home-outro">
+                    <h2>Aptentaciti sociosqu!</h2>
+                    <button>VISIT LEGO</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
