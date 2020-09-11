cfb90d9f5c3e5a27d3a515df66134e6e9e92627a
Carolina Klein <carolina@Carolina-rMBP.local>
2020-09-11 13:57:57 -0400

    added all images to home page

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.scss'
+import logo from '../assets/logo.png'
+
+function Navbar(){
+
+    return(
+        <nav class="navbar nick-nav" role="navigation" aria-label="main navigation">
+            <a class="navbar-item">
+                <img src={logo} width="112" height="28" alt="Bulma"/>
+            </a>
+        </nav>
+    )
+}
+
+export default Navbar;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import '../Home/Home.scss'
-import Footer from '../../components/Footer'
+import '../Home/Home.scss';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import Batman from '../../assets/batman.png';
+import City from '../../assets/city.png';
+import Minecraft from '../../assets/minecraft.png'
+import Thumbnail from '../../assets/thumbnail.png'
 
 function Home() {
 
-    const videos = [1, 2, 3]
+    let videoList = [
+        {
+            firstLogo: City,
+            thumbnail: Thumbnail,
+            available: "Watch Video"
+        },
+        {
+            firstLogo: Minecraft,
+            thumbnail: Thumbnail,
+            available: "Unlock 12/6"
+        },
+        {
+            firstLogo: Batman,
+            thumbnail: Thumbnail,
+            available: "Unlock 12/13"
+        }
+    ]
 
     return (
         <div className="nick-home">
+              <Navbar/>
             <div className="columns is-centered">
                 <div className="column is-half has-text-centered">
                     <h1 className="nick-home-title">
@@ -19,23 +41,25 @@ function Home() {
                 </div>
             </div>
             <div className="columns is-centered">
-                    {videos.map((video) => {
+                    {videoList.map((video) => {
                         return (
-                            <div className="column is-one-quarter has-text-centered" key={video}>
+                            <div className="column is-flex is-one-quarter has-text-centered" key={video}>
                                 <div className="nick-home-cards">
                                         <div>
                                             <img src={video.firstLogo}/>
                                             <img src={video.secondLogo}/>
                                         </div>
-                                        <img src={video.thumbnail}></img>
-                                        <button>Watch Video</button>
+                                        <div>
+                                            <img src={video.thumbnail}></img>
+                                        </div>
+                                        <button>{video.available}</button>
                                     </div>
                           </div>
                         )
                     })}   
             </div>
             <div className="columns is-centered">
-                <div className="column  is-three-quarters has-text-centered">
+                <div className="column is-three-quarters has-text-centered">
                     <div className="nick-home-outro">
                     <h2>Aptentaciti sociosqu!</h2>
                     <button>VISIT LEGO</button>
